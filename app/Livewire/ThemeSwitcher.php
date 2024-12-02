c058bfccefe59ffb7dd45a4cf97ac8cb02fbd616
jade <147321226+mmrnljd@users.noreply.github.com>
2024-12-02 10:19:21 +0800

    activity 4 - edit student, theme switcher

---
+++ app/Livewire/ThemeSwitcher.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Livewire;
+use Livewire\Component;
+class ThemeSwitcher extends Component
+{
+    public function render()
+    {
+        return view('livewire.theme-switcher');
+    }
+}
